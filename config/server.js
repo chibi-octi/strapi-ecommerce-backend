0439613e031d42f026e8eb54b3d06bce6095aba6
Declan Talbert <declan_at@hotmail.com>
2020-12-14 14:37:01 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eac24da8667cef670de4e04d1700ee66'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5016cae6-e3ff-4f23-811d-cc76c9adeae9'
+};
